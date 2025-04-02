c1c9d9c2c5262c4eb96dc45229fce75dbbaeb7b9
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-04-02 08:48:51 +0000

    up

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -8,8 +8,8 @@ const token = process.env.NEXT_PUBLIC_SANITY_TOKEN || ""
 
 export const sanity
  = createClient({
-  projectId,
-  dataset,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
   token
