d82e03653856892873587d74b5f99d84fc51bc52
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-04-02 12:57:43 +0000

    up

--- app/providers/Providers.tsx
+++ app/providers/Providers.tsx
@@ -1,50 +1,48 @@
 "use client";
 
 import { getAuthedUser } from "@/services/auth";
-import React, { useEffect } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useEffect } from "react";
+import { gapi } from "gapi-script";
 import useAuth from "@/stores/auth";
 import { Toaster } from "@/components/ui/toaster";
-import { gapi } from "gapi-script";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
+const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID! || "";
 
-export default function Providers(props: Props) {
-  const { children } = props;
+export default function Providers({ children }: Props) {
   const setAuth = useAuth((state) => state.setAuth);
 
-  const initializeGapi = () => {
-    gapi.client.init({
-      clientId: clientId,
-      scope: "email profile",
-    }).then(() => {
-      console.log('Google API initialized successfully');
-    }).catch((error:any) => {
-      console.error('Error initializing Google API:', error);
-    });
-  };
-
   useEffect(() => {
-    // Load and init google api scripts
+    const initializeGapi = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "email profile",
+      }).then(() => {
+        console.log("Google API initialized successfully");
+      }).catch((error:any) => {
+        console.error("Error initializing Google API:", error);
+      });
+    };
+
     gapi.load("client:auth2", initializeGapi);
   }, []);
 
   useEffect(() => {
     getAuthedUser().then((user) => {
-      if (!user) return;
-      setAuth(user);
-    }).catch((error: Error) => {
-      console.error('Error fetching authenticated user:', error);
+      if (user) {
+        setAuth(user);
+      }
     });
-  }, [setAuth]);
+  }, []);
 
   return (
-    <>
+    <GoogleOAuthProvider clientId={clientId}>
       <Toaster />
       {children}
-    </>
+    </GoogleOAuthProvider>
   );
 }
