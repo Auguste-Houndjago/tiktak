41f27af23044dc4516d1d5637e0b294b9abbccd8
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-04-02 12:44:44 +0000

    up

--- app/providers/Providers.tsx
+++ app/providers/Providers.tsx
@@ -2,7 +2,8 @@
 
 import { getAuthedUser } from "@/services/auth";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+import { gapi } from "gapi-script";
 import useAuth from "@/stores/auth";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -11,14 +12,30 @@ type Props = {
 };
 
 const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID! || "";
-export default function Providers(props: Props) {
-  const { children } = props;
+
+export default function Providers({ children }: Props) {
   const setAuth = useAuth((state) => state.setAuth);
 
   useEffect(() => {
+    const initializeGapi = () => {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "email profile",
+      }).then(() => {
+        console.log("Google API initialized successfully");
+      }).catch((error:any) => {
+        console.error("Error initializing Google API:", error);
+      });
+    };
+
+    gapi.load("client:auth2", initializeGapi);
+  }, []);
+
+  useEffect(() => {
     getAuthedUser().then((user) => {
-      if (!user) return;
-      setAuth(user);
+      if (user) {
+        setAuth(user);
+      }
     });
   }, []);
 
