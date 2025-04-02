917c363addb539e19be838385b851151734b09e4
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-04-02 12:39:04 +0000

    update

--- app/providers/Providers.tsx
+++ app/providers/Providers.tsx
@@ -1,40 +1,31 @@
-"use client"
+"use client";
 
-import { Toaster } from "@/components/ui/toaster"
-import { getAuthedUser } from "@/services/auth"
-import useAuth from "@/stores/auth"
-import { GoogleOAuthProvider } from "@react-oauth/google"
-import { useEffect } from "react"
-
+import { getAuthedUser } from "@/services/auth";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import React, { useEffect } from "react";
+import useAuth from "@/stores/auth";
+import { Toaster } from "@/components/ui/toaster";
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!
+const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID! || "";
 export default function Providers(props: Props) {
-
-  const { children } = props
-  const setAuth = useAuth(state => state.setAuth)
+  const { children } = props;
+  const setAuth = useAuth((state) => state.setAuth);
 
   useEffect(() => {
-    getAuthedUser().then(user => {
+    getAuthedUser().then((user) => {
       if (!user) return;
-    })
+      setAuth(user);
+    });
   }, []);
 
   return (
-    <GoogleOAuthProvider 
-      clientId={clientId}
-      onScriptLoadError={() => {
-        console.error('Google Script Load Error');
-      }}
-      onScriptLoadSuccess={() => {
-        console.log('Google Script Loaded Successfully');
-      }}
-    >
+    <GoogleOAuthProvider clientId={clientId}>
       <Toaster />
       {children}
     </GoogleOAuthProvider>
-  )
+  );
 }
